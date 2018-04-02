3a9d959fb9cd3541d7ba4207d223188eac1b75a5
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-04-02 13:59:39 -0600

    Completed basics screencast on variables and types

---
+++ variables-and-data.js
@@ -0,0 +1,40 @@
+/*
+*Variables, Data Types, and Typing
+ */
+/*let wordData = "Word List";
+
+console.log (wordData);
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax = tax + subTotal;
+*/
+/*
+let obj = {
+    key1:"Value 1",
+    key2: 3,
+    obj2: {
+        obj2Key1 : "value of inner object",
+    },
+};
+
+console.log(obj.obj2.obj2Key1);
+*/
+/*
+let var1 = 2;
+let var2 = 5;
+let result = var1 + var2;
+
+console.log (result);
+*/
+/*
+let add = 1 + 2;
+let sub = 2 - 1;
+let mult = 2*4;
+let division = 4/2;
+let mod = 5 % 2;
+
+let string = "String 1 " + "String 2";
+
+console.log(mod);
+*/
