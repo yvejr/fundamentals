f0a749e68fbaa903cc1f9f691c6a7a578bb52a93
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-04-02 15:06:57 -0600

    Completed basics screencast on objects and APIs

---
+++ objects-apis.js
@@ -0,0 +1,27 @@
+/*
+*Objects, Interfaces, and API's
+*/
+/*
+let dog = {
+    name: "Steve",
+    color: "brown",
+    breed: "pug",
+    size: "small",
+    bark: function (typeOfBark){
+        console.log ("Bark!");
+    },
+};
+*/
+/*
+function x(y) {
+    y.num = y.num + 5;
+    console.log(y);
+}
+
+let y = {
+    name: "Tom",
+    num: 10,
+};
+x(y);
+console.log(y);
+*/
